9006bb2c872f68b899e2291502f19ffad9b45a10
iaalm <iaalmsimon@gmail.com>
2023-06-30 19:48:48 +0800

    setup script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # conf
 My configure file
+
+
+## Setup
+curl https://raw.githubusercontent.com/iaalm/conf/master/setup\_vim.sh | bash
---
+++ setup_vim.sh
@@ -0,0 +1,4 @@
+curl -fLo ~/.vimrc --create-dirs \
+    https://raw.githubusercontent.com/iaalm/conf/master/.vimrc
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
