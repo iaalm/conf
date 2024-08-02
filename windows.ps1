9217cb915380027613e744d0e6b8bdb894e513a7
iaalm <iaalmsimon@gmail.com>
2024-08-02 11:04:02 +0800

    windows.ps1

---
+++ windows.ps1
@@ -0,0 +1,5 @@
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install python311 vim-x64 make jq ripgrep universal-ctags 7zip azcopy ARMClient azure-cli AzureStorageExplorer Everything dejavufonts duplicacy nuget.commandline windows-performance-toolkit
+iwr https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -OutFile ~/_vimrc
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni $HOME/vimfiles/autoload/plug.vim -Force
