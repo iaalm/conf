7450ff5773f334a7ba20843868b35cd8138fc66f
iaalm <iaalmsimon@gmail.com>
2024-08-02 11:09:21 +0800

    Correct README

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ My configure file
 
 ### WSL
 ```bash
-curl https://raw.githubusercontent.com/iaalm/conf/master/wsl.sh | bash
+curl https://raw.githubusercontent.com/iaalm/conf/main/wsl.sh | bash
 ```
 
 ### WSL
 ```shell
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/iaalm/conf/master/windows.sh'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/iaalm/conf/main/windows.ps1'))
 ```
--- windows.ps1
+++ windows.ps1
@@ -1,5 +1,5 @@
 # Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install -y python311 vim-x64 make jq ripgrep universal-ctags 7zip azcopy ARMClient azure-cli AzureStorageExplorer Everything dejavufonts duplicacy nuget.commandline windows-performance-toolkit perfview docker-desktop
+choco install -y python311 vim-x64 make jq ripgrep universal-ctags 7zip azcopy ARMClient azure-cli AzureStorageExplorer Everything dejavufonts duplicacy nuget.commandline windows-performance-toolkit perfview docker-desktop GoogleChrome
 iwr https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -OutFile ~/_vimrc
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
