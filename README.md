00ba3fe870ee90a0b29f9c92e67e0fe181c8cb8d
iaalm <iaalmsimon@gmail.com>
2024-08-19 13:27:57 +0800

    add font

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ My configure file
 curl https://raw.githubusercontent.com/iaalm/conf/main/wsl.sh | bash
 ```
 
-### WSL
+### Windows
 ```shell
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/iaalm/conf/main/windows.ps1'))
 ```
--- windows.ps1
+++ windows.ps1
@@ -1,5 +1,6 @@
 # Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install -y python311 vim-x64 make jq ripgrep universal-ctags 7zip azcopy ARMClient azure-cli AzureStorageExplorer Everything dejavufonts duplicacy nuget.commandline windows-performance-toolkit perfview docker-desktop GoogleChrome
+choco install -y python311 vim-x64 make jq ripgrep universal-ctags 7zip azcopy ARMClient azure-cli AzureStorageExplorer Everything dejavufonts duplicacy nuget.commandline windows-performance-toolkit perfview docker-desktop GoogleChrome nerd-fonts-SourceCodePro
 iwr https://raw.githubusercontent.com/iaalm/conf/main/.vimrc -OutFile ~/_vimrc
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
+echo "set guifont=SauceCodePro_NFM:h12:cANSI:qDRAFT" >> ~/_gvimrc
